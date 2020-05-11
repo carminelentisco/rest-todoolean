4985ddabed725c1abc4ab398af96f4b2196c15c4
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-11 20:36:07 +0200

    JS
    - Aggiunti controlli
    - Aggiunte animazioni
    - Aggiunto delete items

--- script/script.js
+++ script/script.js
@@ -4,25 +4,55 @@ jQuery(document).ready(function() {
     var inputText = $('#input-text');
     var sendButton = $('#send-button');
     var listContainer = $('#todo-item-list')
+    var rule = $('#rule');
+    var buttonRule = $('#button-rule');
+    var todoApp = $('#todo-app');
 
     // Handlebar 
     var source = $('#todo-item-template').html();
     var template = Handlebars.compile(source);
+
+    buttonRule.click(function() {
+        rule.slideUp();
+        setTimeout(function() {
+            todoApp.slideDown();
+        }, 500);
+    });
 
     var myUrl = "http://157.230.17.132:3009/todos";
     ajaxGetCall(myUrl, 'GET' , template, listContainer);
     
     sendButton.on('click', function() {
         var text = inputText.val().trim();
-        ajaxPostCall(myUrl, 'POST' , text, template, listContainer);
+        if ( text !== '') {
+            clearInput(inputText);
+            ajaxPostCall(myUrl, 'POST' , text, template, listContainer);
+        } else {
+            alert('Testo non valido');
+        }
     });
 
+    inputText.keyup(function (e) { 
+        var key = e.which;
+        if ( key == 13) {
+            var text = inputText.val().trim();
+            if ( text !== '') {
+                clearInput(inputText);
+                ajaxPostCall(myUrl, 'POST' , text, template, listContainer);
+            } else {
+                alert('Testo non valido');
+            }
+            
+        }
+    });
 
+    $(document).on('click','.delete', function() {
+        var todoId = $(this).data('id');
+        clearDoom(listContainer);
+        ajaxDeleteCall(myUrl, "DELETE" , todoId, template, listContainer);
+    });
 
-    
-
-}); //____________ End Script Pageù
-
+}); //____________ End Script Page
 
 //____________________ FUNCTIONS _________________//
 
@@ -30,13 +60,14 @@ function ajaxGetCall(myUrl, method , template, listContainer) {
     
     $.ajax({
         url: myUrl,
-        method : method ,
+        method,
         success: function (result) {
             for ( var i = 0; i < result.length; i++ ) {
                 var elementApi = result[i];
 
                 var itemDescription = {
-                    text: elementApi.text
+                    text: elementApi.text,
+                    id: elementApi.id
                 } 
 
                 var html = template(itemDescription);
@@ -52,11 +83,11 @@ function ajaxGetCall(myUrl, method , template, listContainer) {
 
 function ajaxPostCall(myUrl, method , text, template, listContainer) {
     
-    clear(listContainer);
+    clearDoom(listContainer);
 
     $.ajax({
         url: myUrl,
-        method : method ,
+        method,
         data: {
           "text": text  
         },
@@ -66,6 +97,21 @@ function ajaxPostCall(myUrl, method , text, template, listContainer) {
     });
 }
 
-function clear(element) {
+function ajaxDeleteCall(myUrl, method , todoId, template, listContainer) {
+    $.ajax({
+        url: myUrl + '/' + todoId,
+        method,
+        success: function () {
+            
+            ajaxGetCall(myUrl, 'GET' , template, listContainer);
+        }
+    });
+}
+
+function clearDoom(element) {
     element.html('');
+}
+
+function clearInput(element) {
+    element.val('');
 }
